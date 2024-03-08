04f10426e3960cae811c689e46064c02731bab49
LeonardoRubuz <rubuzleonardo@gmail.com>
2024-03-08 12:23:56 +0100

    :hammer::construction: product card stylished

--- src/components/cards/small.jsx
+++ src/components/cards/small.jsx
@@ -2,11 +2,11 @@ function SmallCard() {
     return ( 
         <div className="small-card">
             <div className="front-container">
-                <div>
-                    <span>-30%</span>
+                <div id="badge" style={{ backgroundImage : 'url(/images/image-1.png)' }}>
+                    <span className="circular-badge">-30%</span>
                 </div>
                 <h5>Syltherine</h5>
-                <span>Stylish cafe chair</span>
+                <span id="item-category">Stylish cafe chair</span>
                 <p>45$</p>
             </div>
         </div>
