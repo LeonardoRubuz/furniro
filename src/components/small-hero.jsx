1088afd62801221fab24879eec6ac0e0417853d9
LeonardoRubuz <rubuzleonardo@gmail.com>
2024-03-09 15:34:28 +0100

    :construction: Small hero

---
+++ src/components/small-hero.jsx
@@ -0,0 +1,9 @@
+function SmallHero() {
+    return ( 
+        <section className="small-hero" style={{backgroundImage : 'url(/images/small-hero-bg.png)' }}>
+            <p>Boutique</p>
+        </section>
+     );
+}
+
+export default SmallHero;
--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -1,8 +1,10 @@
 import SmallCard from "../components/cards/small";
+import SmallHero from "../components/small-hero";
 
 function Shop() {
     return ( 
         <>
+            <SmallHero />
             <div className="products-grid">
                 <SmallCard />
                 <SmallCard />
