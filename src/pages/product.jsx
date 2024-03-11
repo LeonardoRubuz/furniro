80118ae3e336704e2e2c874877ce552fc061c2ea
LeonardoRubuz <rubuzleonardo@gmail.com>
2024-03-11 12:59:29 +0100

    :hammer::lipstick: add product page breadcrumb

--- src/pages/product.jsx
+++ src/pages/product.jsx
@@ -1,7 +1,16 @@
 function Product() {
     return ( 
         <>
-            Hey
+            <div className="breadcrumb-container">
+                <div className="breadcrumb">
+                    <p>Accueil</p>
+                    <img src="/images/dash.svg" alt="dash icon" />
+                    <p>Boutique</p>
+                    <img src="/images/dash.svg" alt="dash icon" />
+                    <div></div>
+                </div>
+                <span>Produit</span>
+            </div>
         </>
      );
 }
