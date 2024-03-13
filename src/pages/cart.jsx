09050fbfc873d48eb990fbbeffca0b357ed55b6b
LeonardoRubuz <rubuzleonardo@gmail.com>
2024-03-13 14:31:13 +0100

    :construction: work to be done

--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -5,15 +5,78 @@ function Cart() {
         <>
             <SmallHero />
             <section className="cart">
-                <table>
-
-                </table>
+                <div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Article</th>
+                                <th>Prix</th>
+                                <th>Quantité</th>
+                                <th>Sous-total</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td></td>
+                                <td>Asgaard sofa</td>
+                                <td>10.98 €</td>
+                                <td>1</td>
+                                <td>10.98 €</td>
+                                <td >
+                                    <img src="/images/trash.svg" alt="" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td>Asgaard sofa</td>
+                                <td>10.98 €</td>
+                                <td>1</td>
+                                <td>10.98 €</td>
+                                <td >
+                                    <img src="/images/trash.svg" alt="" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td>Asgaard sofa</td>
+                                <td>10.98 €</td>
+                                <td>1</td>
+                                <td>10.98 €</td>
+                                <td >
+                                    <img src="/images/trash.svg" alt="" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td>Asgaard sofa</td>
+                                <td>10.98 €</td>
+                                <td>1</td>
+                                <td>10.98 €</td>
+                                <td >
+                                    <img src="/images/trash.svg" alt="" />
+                                </td>
+                            </tr>
+                            <tr>
+                                <td></td>
+                                <td>Asgaard sofa</td>
+                                <td>10.98 €</td>
+                                <td>1</td>
+                                <td>10.98 €</td>
+                                <td >
+                                    <img src="/images/trash.svg" alt="" />
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
                 <div className="cart-totals">
                     <h5>Total</h5>
-                    <p>
+                    {/* <p>
                         <span>Sous-total </span>
                         <span style={{color:"#898989"}}> 10.98 €</span>
-                    </p>
+                    </p> */}
                     <p>
                         <span>Total </span>
                         <span style={{color:"#B88E2F", fontWeight:"600"}}> 10.98 €</span>
